0a7e669a2b32d1b0955467e8da6c6b9925b9bd16
ᗰOᑎKEY <39636002+nedesico@users.noreply.github.com>
2025-11-21 23:16:38 +0200

    Modify CustomAccessPoint settings
    Updated the way custom access point can be set, making it easier for users to implement.

--- examples/CustomAccessPoint/CustomAccessPoint.ino
+++ examples/CustomAccessPoint/CustomAccessPoint.ino
@@ -1,20 +1,13 @@
 #include <ESP32_WiFi_Connect.h>
-
-ESP32_WiFi_Connect espwifi; // Initialize the ESP32 WiFi Connect System
+ESP32_WiFi_Connect espwifi;
 
 void setup() {
   Serial.begin(115200);
 
-  espwifi.setCustomAP("MyCustomAPName", "MySup3r5ecurePass@99"); // Set your custom AP SSID and password
-  // Password should be a minimum of 8 characters (secured), or set to "" for an open network (insecure)
-
+  espwifi.debug(true); // Enable ESP32_WiFi_Connect debugging
+  espwifi.setCustomAP("EasyTemps"); // Insecure Custom Access Point SSID, without password (insecure open network)
+  //espwifi.setCustomAP("EasyTemps", "S3curePa55"); // Custom Access Point SSID, secured with password
   espwifi.begin();
-
-  Serial.println(espwifi.getStatus()); // Get the device WiFi status.
-  // wifi_not_configured, means the WiFi SSID hasn't been configured yet
-  // wifi_connection_failed, means the WiFi SSID has been configured, but it can't connect.
-  // - Check your settings, or whether the WiFi is out of range. Alternatively factory reset or format flash memory and reconfigure the device.
-  // wifi_connected, means the WiFi SSID has been configured and is connected
 }
 
 void loop() {}
